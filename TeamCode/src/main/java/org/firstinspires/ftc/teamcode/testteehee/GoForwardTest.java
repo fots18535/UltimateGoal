14d19a4438819a9b82a1be40fdf80ecfdbe43e71
Andy Fisher <afisher2@users.noreply.github.com>
2021-01-03 18:40:20 -0600

    Robot Refactor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testteehee/GoForwardTest.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.testteehee;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.HunkOfMetal;
+
+@Autonomous (group = "testteehee")
+public class GoForwardTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        HunkOfMetal hunk = new HunkOfMetal(this);
+        hunk.initialize();
+
+        waitForStart();
+
+        hunk.forward(1, 30);
+        hunk.arret();
+    }
+}
