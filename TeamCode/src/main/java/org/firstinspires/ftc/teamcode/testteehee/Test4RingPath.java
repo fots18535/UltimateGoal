d70451d5a1d406db4bb56bd668d994530ebe1852
Andy Fisher <afisher2@users.noreply.github.com>
2021-03-05 18:34:01 -0600

    ring paths

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testteehee/Test4RingPath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testteehee/Test4RingPath.java
@@ -6,24 +6,22 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.HunkOfMetal;
 
 @Autonomous
-public class TestZeroRingPath extends LinearOpMode {
+public class Test4RingPath extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         HunkOfMetal hunk = new HunkOfMetal(this);
         hunk.initialize();
         waitForStart();
-
         hunk.forward(1, 60);
         hunk.throwRings();
-        hunk.forward(1, 8);
-        hunk.turnLeft(180, 0.8);
-       // hunk.chaChaRealSmooth(-1, 20);
+        hunk.turnLeft(180,1);
+        hunk.forward(-1,60);
         hunk.wristDown();
         sleep(500);
         hunk.clawOpen();
         sleep(800);
         hunk.wristUp();
-       // hunk.chaChaRealSmooth(1, 12);
-        hunk.arret();
+        hunk.forward(1,42);
+
     }
 }
